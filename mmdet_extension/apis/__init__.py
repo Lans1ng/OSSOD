9ff7d263f4334f2e376437a31dab650ddf178b91
Liu Nanqing <lansing163@163.com>
2024-04-08 15:20:53 +0800

    init

--- mmdet_extension/apis/__init__.py
+++
@@ -1,2 +0,0 @@
-from .test import single_gpu_test, multi_gpu_test
-from .train import train_detector
--- mmdet_extension/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .backbones import *
-from .roi_head import *
-from .loss import *
-from .detectors import *
